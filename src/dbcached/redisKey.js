0649d43e037a78fca883ff7698e0118a936b9c54
windsome.feng <36081189@qq.com>
2020-08-11 16:44:28 +0800

    修改redis中key为按字母序的字符串.

--- src/dbcached/redisKey.js
+++ src/dbcached/redisKey.js
@@ -1,5 +1,11 @@
 import _debug from 'debug';
 const debug = _debug('yh:mongo:dbcached:redisKey');
+import stringify from 'json-stable-stringify';
+
+function stableSort(a, b) {
+  return a.key.localeCompare(b.key);
+  // return a.key < b.key ? -1 : 1;
+}
 
 export const SEPARATOR = '##';
 /**
@@ -15,13 +21,16 @@ export function getRedisKey(model, type, where_data, sort_data) {
     //当为数据记录型key时,where_data为_id
     r_key = model + SEPARATOR + 'd' + SEPARATOR + where_data;
   } else if (type == 's' || type == 'w' || type == 'c') {
+    //为数据列表查询型数据. s表示列表查询,c表示列表查询总数,w表示单条查询条件
     r_key = model + SEPARATOR + type + SEPARATOR;
     if (where_data) {
-      r_key += JSON.stringify(where_data);
+      // r_key += JSON.stringify(where_data);
+      r_key += stringify(where_data, stableSort)
     }
     r_key += SEPARATOR;
     if (sort_data) {
-      r_key += JSON.stringify(sort_data);
+      // r_key += JSON.stringify(sort_data);
+      r_key += stringify(sort_data, stableSort)
     }
   } else {
     debug('error! not support type:' + type);
