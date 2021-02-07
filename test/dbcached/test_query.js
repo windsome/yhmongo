5e994caf79a8b6f14aa4253fbc2699ef1447b1dd
windsome.feng <36081189@qq.com>
2021-02-07 20:45:40 +0800

    fix itemFulfillQuery

--- test/dbcached/test_query.js
+++ test/dbcached/test_query.js
@@ -30,6 +30,14 @@ let item2 = {
     count: 2000
   }
 }
+let item3 = {
+  _id: "5fe31a131111111111000002",
+  brief: "监听定时器5ffd331a2222222222000001.timestamp=[8,20]及5ffd331a2222222222000002.switch_1=true时报警",
+  depend: ["5ffd331a2222222222000001", "5ffd331a2222222222000002"],
+  name: "测试报警器2",
+  owner: "5fe31b131111111111000001",
+  status: 0,
+}
 let queries = [
   { company: '5b210fea77c0a27c84c0ea23' },
   { vehicle: '京A12345' },
@@ -45,18 +53,27 @@ let queries = [
   },
   { createdAt: { $gt: 1544631840000, $lt: 1544631940000 } },
 ]
+let queries2 = [
+  { depend: "5ffd331a2222222222000001"}
+]
 
 let start = new Date().getTime();
-debug('deal item1')
+debug('deal item1:', item);
 queries.map(q => {
   let start = new Date().getTime();
   let result = query.itemFulfillQuery(item, q);
-  debug('query', (new Date().getTime() - start)/1000, result, q);
+  debug('query1', (new Date().getTime() - start)/1000, result, JSON.stringify(q));
 });
-debug('deal item2\n\n')
+debug('\n\ndeal item2: ', item2);
 queries.map(q => {
   let start = new Date().getTime();
   let result = query.itemFulfillQuery(item2, q);
-  debug('query', (new Date().getTime() - start)/1000, result, q);
+  debug('query2', (new Date().getTime() - start)/1000, result, JSON.stringify(q));
+});
+debug('\n\ndeal item3: ', item3);
+queries2.map(q => {
+  let start = new Date().getTime();
+  let result = query.itemFulfillQuery(item3, q);
+  debug('query3', (new Date().getTime() - start)/1000, result, JSON.stringify(q));
 });
 debug('time elapse', (new Date().getTime() - start)/1000);
